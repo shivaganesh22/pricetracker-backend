b9266a427c367f8432646e5ec2fc8ac3a3362090
shivaganesh22 <rockstarshivaganesh@gmail.com>
2024-05-09 13:01:46 +0530

    s

--- api/views.py
+++ api/views.py
@@ -75,15 +75,19 @@ class SearchView(APIView):
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+chrome_options=Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 class TestView(APIView):
     def get(self, request):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
         driver.get("https://pricehistoryapp.com/")
 
         # Wait for the search input field to be clickable
